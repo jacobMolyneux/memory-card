b9abf8521819a319df1f80ac49708da11181995b
Jacob Molyneux <79246480+jacobMolyneux@users.noreply.github.com>
2021-06-05 22:48:54 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from './Cards/cards.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Card className = "card"id = "1"/>
+      <Card className = "card" id = "2"/>
+      <Card className = "card" id = "3"/>
+      <Card className = "card" id = "4"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Cards/cards.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './cards.css'
+
+export default class Card extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            names: ['hello']
+        }
+        this.CardClicked = () => {
+            console.log("card is clicked");
+        }
+    }
+    render(){
+        return(
+            <div onClick = {this.CardClicked} className = 'characterCardDiv'>
+                <div className = 'logo'>Logo</div>
+                <div className = 'Name'>
+                    <h1>{this.state.names}</h1>
+                </div>
+            </div>
+        )
+    }
+}
